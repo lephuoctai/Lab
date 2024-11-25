b784a0c60f502bf554f84de16048a64b2753c371
Le Phuoc Tai <lephuoctai2006@gmail.com>
2024-11-25 23:20:02 +0700

    beta-4.2
    struct data

--- Lab 03/main.js
+++ Lab 03/main.js
@@ -1,56 +1,64 @@
 const data = [
     {
         order: 1,
-        name: 'MO231 - Pomeranian Trắng',
+        code: 'MO231',
+        name: 'Pomeranian Trắng',
         gender: 1,
         age: '02',
         price: '6.900.000'
     },
     {
         order: 2,
-        name: 'MO502 - Poodle Tiny Vàng',
+        code: 'MO502',
+        name: 'Poodle Tiny Vàng',
         gender: 0,
         age: '02',
         price: '3.900.000'
     },
     {
         order: 3,
-        name: 'MO102 - Poodle Tiny Sepia',
+        code: 'MO102',
+        name: 'Poodle Tiny Sepia',
         gender: 0,
         age: '02',
         price: '4.000.000'
     },
     {
         order: 4,
-        name: 'MO512 - Alaskan Malamute',
+        code: 'MO512',
+        name: 'Alaskan Malamute',
         gender: 1,
         age: '02',
         price: '8.900.000'
     },
     {
         order: 5,
-        name: 'MO231 - Pembroke Corgi',
+        code: 'MO231',
+        name: 'Pembroke Corgi',
         gender: 1,
         age: '03',
         price: '6.500.000'
     },
     {
         order: 6,
-        name: 'MO502 - Pembroke Corgi',
+        code: 'MO502',
+        name: 'Pembroke Corgi',
         gender: 1,
         age: '02',
         price: '5.900.000'
     },
     {
         order: 7,
-        name: 'MO231 - Pomeranian Trắng',
+        code: 'MO231',
+        name: 'Pomeranian Trắng',
         gender: 0,
         age: '02',
         price: '6.900.000'
     },
     {
         order: 8,
-        name: 'MO512 - Poodle Tiny',
+        code: 'MO102',
+        name: 'Poodle Tiny',
         gender: 1,
         age: '03',
         price: '7.900.000'
@@ -64,7 +72,7 @@ const item = data.map((item) => {
             <img src="./photos/dogs/dog${item.order}.jpg" alt="" class="item__img">
             <div class="item__content">
                 <div class="item__content__name">
-                    <h5>${item.name}</h5>
+                    <h5>${item.code} - ${item.name}</h5>
                 </div>
                 <div class="item__content__gender">
                     Giống: ${genderShow}
