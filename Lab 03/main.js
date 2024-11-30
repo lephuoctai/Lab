160244e436e6d55efe5a21a0578f3b7033908574
Le Phuoc Tai <lephuoctai2006@gmail.com>
2024-12-01 00:09:48 +0700

    beta-4.5
    comple Selldog

--- Lab 03/main.js
+++ Lab 03/main.js
@@ -63,7 +63,24 @@ const data = [
         age: '03',
         price: '7.900.000'
     }
-]
+];
+const data2 = [
+    {
+        order: 1,
+        title: "Pomeranian là gì? Cách nhận biết chó Pomeranian",
+        info: "Pomeranian hay còn gọi là chó Pomeranian (chó Pom) luôn nằm trong top những thú cưng dễ thương nhất. Không chỉ vậy, giống chó xiế.."
+    },
+    {
+        order: 2,
+        title: "Chế độ ăn cho chó bạn cần biết",
+        info: "Việc chia khẩu phần ăn cho chó thoạt nghe có vẻ đơn giản nhưng có một số quy tắc bạn nên biết để chó cưng dễ dàng hấp thụ các chất.."
+    },
+    {
+        order: 3,
+        title: "Tại sao chó cắn phá đồ đạc và cách phòng ngừa hiệu quả",
+        info: "Chó cắn là hiện tượng phổ biến trong quá trình phát triển. Tuy nhiên, không ai muốn thấy đồ đạc, vật dụng quang trọng của mình bị chó cắn."
+    }
+];
 
 const item = data.map((item) => {
     let genderShow = (item.gender)? 'Đực' : 'Cái';
@@ -89,3 +106,34 @@ const item = data.map((item) => {
     `;
 });
 document.querySelector('.section2__list').innerHTML += item.join('');
+
+const item2 = data2.map((item) => {
+    return `
+        <div class="section4__list__item">
+            <div class="section4__list__item__imgInner">
+                <img src="./photos/dogs/dogss3${item.order}.jpg" alt="">
+            </div>
+            <div class="section4__list__item__content">
+                <div class="section4__list__item__content__tag">
+                    <h3>Kiến thức thú cưng</h3>
+                </div>
+                <div class="section4__list__item__content__title">
+                    <h5>${item.title}</h5>
+
+                </div>
+                <div class="section4__list__item__content__paragraph">
+                    <p>${item.info}</p>
+                </div>
+            </div>
+        </div>
+    `
+});
+document.querySelector('.section4__list').innerHTML += item2.join('');
+document.querySelectorAll('.section4__list, .section2__list').forEach((element) => {
+    element.innerHTML += `
+    <button class="section2__list__more">
+        <h4>Xem thêm</h4>
+        <i class="fa-solid fa-chevron-right"></i>
+    </button>
+`;
+})
