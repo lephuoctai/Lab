f0074e38bed83af2e59e55f12a710f2fc5b25ae6
Le Phuoc Tai <lephuoctai2006@gmail.com>
2024-12-05 23:38:57 +0700

    beta-5
    MC web comple section 2

--- Lab TH/main.js
+++ Lab TH/main.js
@@ -1,17 +1,40 @@
-const getStudentID = document.querySelector('.StudentID');
-const Student = Array(100).fill(0);
-
-document.querySelector('button#Send').addEventListener('click', () => {
-    let ID = getStudentID.value;
-    console.log(ID);
-
-    if(Student[ID] == 1)
+dataGameList = [
     {
-        alert("Da ton tai!");
-    }
-    else
+        order: 1,
+        game: "Bedwar",
+        playerOnline: 56,
+        version: "1.14+",
+    },
     {
-        Student[ID] = 1;
-        alert("Them thanh cong!");
+        order: 2,
+        game: "Skywar",
+        playerOnline: 32,
+        version: "1.18+",
+    },
+    {
+        order: 3,
+        game: "Eggwar",
+        playerOnline: 25,
+        version: "1.12+",
     }
-})
+];
+
+const gameList = dataGameList.map((item) => {
+    return `
+     <li class="section2__gameList__item item">
+        <div class="section2__gameList__item__imgInner">
+            <img src="./photos/game${item.order}.png" alt="">
+        </div>
+        <div class="section2__gameList__item__content">
+            <div class="section2__gameList__item__content__title">
+                <h3 class="primaryTitle">${item.game}</h3>
+                <div class="section2__gameList__item__content__title__status">
+                    <h6 class="playerOnline subTitle">${item.playerOnline} online</h6>
+                    <p style="letter-spacing: 2px;">ver: ${item.version}</p>
+                </div>
+            </div>
+        </div>
+    </li>
+    `;
+});
+document.querySelector(".section2__gameList").innerHTML += gameList.join("");
