e53e56e8b89f3cdf9998a502c8b0140cf78a76a8
Le Phuoc Tai <lephuoctai2006@gmail.com>
2024-12-17 23:03:35 +0700

    beta-6.8
    link screen 01 - screen 02

--- Lab AI/main.js
+++ Lab AI/main.js
@@ -43,6 +43,64 @@ const boardProgress =
             </div>
         </div>
     `;
-
+const chatBox = 
+    `
     
-// selectE('.phone').innerHTML = boardProgress;
+        <div class="topBoard">
+            <div class="topBoard__background">
+                <img src="./Photos/board__background.png" alt="">
+            </div>
+            <div class="topBoard__content">
+                <div class="topBoard__buttonContainer">
+                    <button onclick="selectE('.phone').innerHTML = boardProgress" class="topBoard__buttonContainer__back">
+                        <i class="fa-solid fa-arrow-left-long"></i>
+                    </button>
+                    <button class="topBoard__buttonContainer__settings">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </button>
+                </div>
+                <div class="topBoard__logo">
+                    <img src="./Photos/logo2.png" alt="">
+                </div>
+
+            </div>
+        </div>
+        <div class="phoneView">
+            <div class="chatBox">
+                <ul class="chatBoxDizyList">
+                    <li class="chatBoxDizyList__message">Hi Hedy! Dizy here!</li>
+                </ul>
+                <ul class="chatBoxUserList">
+                    <li class="chatBoxUserList__message">Hello Dizy!</li>
+                    <li class="chatBoxUserList__message">How are you? Good?</li>
+                </ul>
+                <ul class="chatBoxDizyList">
+                    <li class="chatBoxDizyList__message">I’m fantastic!</li>
+                </ul>
+                <ul class="chatBoxUserList">
+                    <li class="chatBoxUserList__message">I’ve just broke up with my GF :(</li>
+                </ul>
+                <ul class="chatBoxDizyList">
+                    <li class="chatBoxDizyList__message">Sorry to hear that, don’t be sad</li>
+                </ul>
+                </ul>
+                <ul class="chatBoxUserList">
+                    <li class="chatBoxUserList__message">I'm boring now</li>
+                    <li class="chatBoxUserList__message">Recomend me some destination!</li>
+                </ul>
+                <ul class="chatBoxDizyList">
+                    <li class="chatBoxDizyList__message">Here are some exciting destinations for you to explore based on
+                        your interests! https://www.phuquocislandguide.com/</li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="sendBox">
+            <input id="inputSendBox" type="text" placeholder="Type a message ...">
+            <button class="sendBox__sendButton secondaryButton borderSquare">
+                <i class="fa-regular fa-paper-plane"></i>
+            </button>
+        </div>
+    `;
+    
+selectE('.phone').innerHTML = boardProgress;
